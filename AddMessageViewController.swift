90dd4051187401665819ca4a412101ce66f9fdbd
Cordavi <michaelamundsen@gmail.com>
2016-07-20 16:40:00 -0400

    Fixed file location in project

---
+++ AddMessageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddMessageViewController.swift
+//  SlapChat
+//
+//  Created by Michael Amundsen on 7/20/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
